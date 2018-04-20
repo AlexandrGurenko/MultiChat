c16dfb81a35cf05566ba51ee5f9aa9b6cde02d2e
AlexandrGurenko <gurenko.alexandr@gmail.com>
2018-04-20 11:35:49 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace MultiChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.connectedState = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.messBox = new System.Windows.Forms.TextBox();
+            this.disconnectBtn = new System.Windows.Forms.Button();
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.userNameBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chatView = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.connectedState);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.sendBtn);
+            this.groupBox1.Controls.Add(this.messBox);
+            this.groupBox1.Controls.Add(this.disconnectBtn);
+            this.groupBox1.Controls.Add(this.connectBtn);
+            this.groupBox1.Controls.Add(this.userNameBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(179, 357);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Управление";
+            // 
+            // connectedState
+            // 
+            this.connectedState.AutoSize = true;
+            this.connectedState.ForeColor = System.Drawing.Color.Red;
+            this.connectedState.Location = new System.Drawing.Point(82, 69);
+            this.connectedState.Name = "connectedState";
+            this.connectedState.Size = new System.Drawing.Size(73, 13);
+            this.connectedState.TabIndex = 8;
+            this.connectedState.Text = "Disconnected";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Состояние:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Сообщение:";
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Location = new System.Drawing.Point(7, 327);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(166, 23);
+            this.sendBtn.TabIndex = 5;
+            this.sendBtn.Text = "Send";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // messBox
+            // 
+            this.messBox.Location = new System.Drawing.Point(7, 109);
+            this.messBox.Multiline = true;
+            this.messBox.Name = "messBox";
+            this.messBox.Size = new System.Drawing.Size(166, 212);
+            this.messBox.TabIndex = 4;
+            // 
+            // disconnectBtn
+            // 
+            this.disconnectBtn.Location = new System.Drawing.Point(98, 43);
+            this.disconnectBtn.Name = "disconnectBtn";
+            this.disconnectBtn.Size = new System.Drawing.Size(75, 23);
+            this.disconnectBtn.TabIndex = 3;
+            this.disconnectBtn.Text = "Disconnect";
+            this.disconnectBtn.UseVisualStyleBackColor = true;
+            this.disconnectBtn.Click += new System.EventHandler(this.disconnectBtn_Click);
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.Location = new System.Drawing.Point(6, 43);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(75, 23);
+            this.connectBtn.TabIndex = 2;
+            this.connectBtn.Text = "Connect";
+            this.connectBtn.UseVisualStyleBackColor = true;
+            this.connectBtn.Click += new System.EventHandler(this.connectBtn_Click);
+            // 
+            // userNameBox
+            // 
+            this.userNameBox.Location = new System.Drawing.Point(40, 17);
+            this.userNameBox.Name = "userNameBox";
+            this.userNameBox.Size = new System.Drawing.Size(133, 20);
+            this.userNameBox.TabIndex = 1;
+            this.userNameBox.Tag = "";
+            this.userNameBox.Text = "Сашка";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ник:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chatView);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(179, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(363, 357);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Уютный чатик";
+            // 
+            // chatView
+            // 
+            this.chatView.BackColor = System.Drawing.Color.White;
+            this.chatView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chatView.Location = new System.Drawing.Point(3, 16);
+            this.chatView.Multiline = true;
+            this.chatView.Name = "chatView";
+            this.chatView.ReadOnly = true;
+            this.chatView.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.chatView.Size = new System.Drawing.Size(357, 338);
+            this.chatView.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 357);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Многопользовательский чат";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button disconnectBtn;
+        private System.Windows.Forms.Button connectBtn;
+        private System.Windows.Forms.TextBox userNameBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.TextBox messBox;
+        private System.Windows.Forms.TextBox chatView;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label connectedState;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace MultiChat
+{
+    public partial class Form1 : Form
+    {
+        int port;
+        IPAddress ip;
+        IPEndPoint ep;
+        Socket senderSocket;
+        Thread listenThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+            port = 9005;
+            ip = IPAddress.Parse("224.7.7.7");
+            ep = new IPEndPoint(ip, port);
+            try
+            {
+                senderSocket = new Socket(AddressFamily.InterNetwork,
+                                    SocketType.Dgram,
+                                    ProtocolType.Udp);
+
+                senderSocket.SetSocketOption(SocketOptionLevel.IP,
+                                       SocketOptionName.MulticastTimeToLive, 1);
+
+                senderSocket.SetSocketOption(SocketOptionLevel.IP,
+                                       SocketOptionName.AddMembership,
+                                       new MulticastOption(ip));
+
+                listenThread = new Thread(new ThreadStart(Listen));
+                listenThread.IsBackground = true;
+            }catch(Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void Listen()
+        {
+            try
+            {
+                while (true)
+                {
+                    Socket receiverSocket = new Socket(AddressFamily.InterNetwork,
+                                                       SocketType.Dgram,
+                                                       ProtocolType.Udp);
+
+                    IPEndPoint epx = new IPEndPoint(IPAddress.Any, 9005);
+                    receiverSocket.Bind(epx);
+
+                    receiverSocket.SetSocketOption(SocketOptionLevel.IP,
+                                       SocketOptionName.AddMembership,
+                                       new MulticastOption(ip, IPAddress.Any));
+
+                    byte[] buff = new byte[1024];
+                    receiverSocket.Receive(buff);
+
+                    string mess = Encoding.UTF8.GetString(buff);
+                    chatView.Text += mess + "\r\n";
+                    receiverSocket.Close();
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Ошибочка: " + err.Message);
+            }
+        }
+
+        private void SendMessage(string mess)
+        {
+            senderSocket.Send(Encoding.UTF8.GetBytes(mess));
+        }
+
+        private void connectBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!senderSocket.Connected)
+                {
+                    senderSocket = new Socket(AddressFamily.InterNetwork,
+                                    SocketType.Dgram,
+                                    ProtocolType.Udp);
+
+                    senderSocket.SetSocketOption(SocketOptionLevel.IP,
+                                           SocketOptionName.MulticastTimeToLive, 1);
+
+                    senderSocket.SetSocketOption(SocketOptionLevel.IP,
+                                           SocketOptionName.AddMembership,
+                                           new MulticastOption(ip));
+                    listenThread = new Thread(new ThreadStart(Listen));
+                    listenThread.IsBackground = true;
+
+                    senderSocket.Connect(ep);
+                    connectedState.ForeColor = Color.Green;
+                    connectedState.Text = "Connected";
+                    listenThread.Start();
+                    SendMessage($"{DateTime.Now.ToShortTimeString()}: {userNameBox.Text} присоединился к уютному чатику:)\r\n");
+                }
+
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void disconnectBtn_Click(object sender, EventArgs e)
+        {
+            SendMessage($"{DateTime.Now.ToShortTimeString()}: {userNameBox.Text} уходит от нас:(\r\n");
+            senderSocket.Close();
+            senderSocket.Dispose();
+            connectedState.ForeColor = Color.Red;
+            connectedState.Text = "Disconnected";
+        }
+
+        private void sendBtn_Click(object sender, EventArgs e)
+        {
+            if (senderSocket.Connected)
+            {
+                if (messBox.Text != "")
+                    SendMessage($"{DateTime.Now.ToShortTimeString()}: {userNameBox.Text}: {messBox.Text}\r\n");
+                messBox.Clear();
+                messBox.Focus();
+            }
+        }
+    }
+}
